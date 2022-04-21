ea42a9b621cd374e9484000b6c25cf2512f0082e
muhammad iqbal <iqbal.muh998@gmail.com>
2022-04-21 09:45:49 +0700

    commit 3

---
+++ seeders/20220421012941-seed-Products.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   const products = require('../data/products.json')
+   products.forEach(el => {
+     el.createdAt = new Date()
+     el.updatedAt = new Date()
+     return el
+   })
+   return queryInterface.bulkInsert('Products', products, {})
+  },
+
+  down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     return queryInterface.bulkDelete('Products', null, {})
+  }
+};
